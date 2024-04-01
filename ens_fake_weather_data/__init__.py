ddb29d6b11680870572668f41e57264dbdc9096d
ernisol <erwan.nisol@free.fr>
2024-04-01 18:10:08 +0200

    Initial commit

---
+++ ens_fake_weather_data/__init__.py
@@ -0,0 +1,4 @@
+"""Top-level package for ens-fake-weather-data."""
+
+__author__ = """Erwan Nisol"""
+__email__ = "erwan.nisol@free.fr"
---
+++ setup.py
@@ -0,0 +1,5 @@
+"""Allows for package installation with older setuptools versions."""
+
+from setuptools import setup
+
+setup(name=ens_fake_weather_data, version="0.0.0")
